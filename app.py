a6ae41077d9bd6d36aa2bddb7c658506fad605ca
Kauan-Lobao <137134749+Kauan-Lobao@users.noreply.github.com>
2025-08-26 23:55:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ def soma(a: int, b: int) -> int:
     return a + b #retorna a+b
 
 if __name__ == "__main__":
-    print("Resultado da soma:", soma(2, 3))
+    print("Resultado da soma:", soma(2, 3)) #resultado    
