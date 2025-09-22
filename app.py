2359fc0a4b6afc4b9fbec92bc649cbc15133886c
Kauan-Lobao <137134749+Kauan-Lobao@users.noreply.github.com>
2025-09-22 12:56:47 -0300

    Update and rename app.py to run.py

--- app.py
+++
@@ -1,7 +0,0 @@
-def soma(a: int, b: int) -> int:
-    if not isinstance(a, int) or not isinstance(b, int):
-        raise ValueError("Ambos os valores devem ser inteiros")
-    return a + b  # retorna a + b
-
-if __name__ == "__main__":
-    print("Resultado da soma:", soma(2, 3))  # resultado
